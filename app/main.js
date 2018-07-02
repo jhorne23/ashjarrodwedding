b02d8faa75aca9a422ff0ba00857dadeef32a72d
Jarrod Horne <jarrod.horne@gmail.com>
2018-07-02 13:42:12 +1000

    Clean repo and remove old unused resources etc

--- app/main.js
+++
@@ -1,6 +0,0 @@
-import Header from "./header.jsx"
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
